b6c83a4356a58e4275bc79821c8a76fb96a2fef5
vshowc <vshowc@outlook.com>
2023-05-07 11:52:40 +0800

    Interceptor function design

---
+++ handler/intercept.go
@@ -0,0 +1,15 @@
+// Interceptor
+
+package handler
+
+import "net/http"
+
+// Interceptor Abstract Method
+// Returning true indicates a release request, while false indicates the opposite.
+type Interceptor func(http.ResponseWriter, *http.Request) bool
+
+// Default Interceptor Method
+// This method does not have any interception processing.
+func defaultInterceptor(http.ResponseWriter, *http.Request) bool {
+	return true
+}
