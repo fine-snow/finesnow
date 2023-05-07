df77d73471f467e3d6a60976bc638596513eff13
vshowc <vshowc@outlook.com>
2023-05-07 12:02:03 +0800

    intercept parameter functional annotation supplement

--- snow/run.go
+++ snow/run.go
@@ -9,7 +9,7 @@ import (
 
 // Application Framework Launch Method
 // addr Start address parameter, for example: 127.0.0.1:8088
-// intercept Global interceptor parameter
+// intercept Global interceptor parameter, if the interceptor function is not required, this parameter can be passed to nil
 func Application(addr string, intercept handler.Interceptor) {
 	handle := handler.NewHandle()
 	handle.SetIntercept(intercept)
