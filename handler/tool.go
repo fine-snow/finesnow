7b681c0dc346773535687fb20b000d61e97f5e91
vshowc <vshowc@outlook.com>
2023-05-07 18:38:36 +0800

    Optimizing the processing logic for numbers and file parameters in the HTTP input parameter processing method

--- handler/tool.go
+++ handler/tool.go
@@ -53,11 +53,21 @@ func dealInParam(paramNames []string, rt reflect.Type, values url.Values, files 
 	for i, k := range paramNames {
 		t := rt.In(i)
 		if t.String() == "*multipart.FileHeader" {
-			in = append(in, reflect.ValueOf(files[k][0]))
+			headers := files[k]
+			if headers != nil {
+				in = append(in, reflect.ValueOf(headers[0]))
+			} else {
+				in = append(in, reflect.ValueOf(&multipart.FileHeader{}))
+			}
 			continue
 		}
 		if t.String() == "multipart.FileHeader" {
-			in = append(in, reflect.ValueOf(*files[k][0]))
+			headers := files[k]
+			if headers != nil {
+				in = append(in, reflect.ValueOf(*(headers[0])))
+			} else {
+				in = append(in, reflect.ValueOf(multipart.FileHeader{}))
+			}
 			continue
 		}
 		if t.String() == "[]*multipart.FileHeader" {
@@ -74,16 +84,25 @@ func dealInParam(paramNames []string, rt reflect.Type, values url.Values, files 
 		}
 		switch t.Kind() {
 		case reflect.Bool:
-			v, err := strconv.ParseBool(values.Get(k))
-			if err != nil {
-				panic(err)
-			}
+			v, _ := strconv.ParseBool(values.Get(k))
 			in = append(in, reflect.ValueOf(v))
 		case reflect.String:
 			in = append(in, reflect.ValueOf(values.Get(k)))
-		case reflect.Float32, reflect.Float64,
-			reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			v, _ := strconv.ParseInt(values.Get(k), 0, 64)
+			elem := reflect.New(t).Elem()
+			elem.SetInt(v)
+			in = append(in, elem)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			v, _ := strconv.ParseUint(values.Get(k), 0, 64)
+			elem := reflect.New(t).Elem()
+			elem.SetUint(v)
+			in = append(in, elem)
+		case reflect.Float32, reflect.Float64:
+			v, _ := strconv.ParseFloat(values.Get(k), 64)
+			elem := reflect.New(t).Elem()
+			elem.SetFloat(v)
+			in = append(in, elem)
 		default:
 			panic("argument out of range")
 		}
