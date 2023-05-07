a9931d9db7fbd17555f8827c237cf681440dac51
vshowc <vshowc@outlook.com>
2023-05-07 11:59:56 +0800

    framework launch method design

---
+++ snow/run.go
@@ -0,0 +1,21 @@
+// Framework Launch Method
+
+package rain
+
+import (
+	"github.com/fine-snow/finesnow/handler"
+	"net/http"
+)
+
+// Application Framework Launch Method
+// addr Start address parameter, for example: 127.0.0.1:8088
+// intercept Global interceptor parameter
+func Application(addr string, intercept handler.Interceptor) {
+	handle := handler.NewHandle()
+	handle.SetIntercept(intercept)
+	http.Handle("/", handle)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		panic(err)
+	}
+}
