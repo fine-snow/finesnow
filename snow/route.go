6c3748c7eb76200389b5c18f7e45d404e66a0a91
vshowc <vshowc@outlook.com>
2023-05-07 19:01:08 +0800

    route addition method

---
+++ snow/route.go
@@ -0,0 +1,15 @@
+// Route addition method
+
+package snow
+
+import "github.com/fine-snow/finesnow/router"
+
+// AddGetRoute Add a routing method for get requests
+func AddGetRoute(url string, fun interface{}) {
+	router.AddRoute(url, fun, router.HttpMethodGet)
+}
+
+// AddPostRoute Add a routing method for post requests
+func AddPostRoute(url string, fun interface{}) {
+	router.AddRoute(url, fun, router.HttpMethodPost)
+}
