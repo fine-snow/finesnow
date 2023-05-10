0c754d621bd6b13fecc6647a27e38f4e026cfeca
vshowc <vshowc@outlook.com>
2023-05-10 08:16:38 +0800

    Start method tuning

--- snow/run.go
+++ snow/run.go
@@ -11,8 +11,7 @@ import (
 // addr Start address parameter, for example: 127.0.0.1:8088
 // intercept Global interceptor parameter, if the interceptor function is not required, this parameter can be passed to nil
 func Run(addr string, intercept handler.Interceptor) {
-	handle := handler.NewHandle()
-	handle.SetIntercept(intercept)
+	handle := handler.NewHandle(intercept)
 	http.Handle("/", handle)
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
