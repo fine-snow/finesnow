ac1cdb491924df404d13a4e94318b99c2af9c42e
vshou <vshowc@outlook.com>
2023-06-11 15:21:00 +0800

    ListenAndServe start-up mode transformation

--- snow/run.go
+++ snow/run.go
@@ -13,9 +13,8 @@ import (
 // intercept Global interceptor parameter, if the interceptor function is not required, this parameter can be passed to nil
 func Run(addr string, intercept handler.Interceptor) {
 	handle := handler.NewHandle(intercept)
-	http.Handle("/", handle)
 	fmt.Println("\n    _______           _____                    \n   / ____(_)___  ___ / ___/____  ____ _      __\n  / /_  / / __ \\/ _ \\\\__ \\/ __ \\/ __ \\ | /| / /\n / __/ / / / / /  __/__/ / / / / /_/ / |/ |/ / \n/_/   /_/_/ /_/\\___/____/_/ /_/\\____/|__/|__/  \n                                               ")
-	err := http.ListenAndServe(addr, nil)
+	err := http.ListenAndServe(addr, handle)
 	if err != nil {
 		panic(err)
 	}
