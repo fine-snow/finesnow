337c317df3d7e4d029dc4018ae55129c008a3cc5
vshou <vshowc@outlook.com>
2023-06-17 11:46:52 +0800

    HTTP public parameter optimization

--- router/http_param.go
+++ router/http_param.go
@@ -2,24 +2,14 @@
 
 package router
 
-type httpMethod string
 type httpContentType string
 
 var (
-	HttpMethodGet     = new(httpMethod)
-	HttpMethodPost    = new(httpMethod)
-	HttpMethodPut     = new(httpMethod)
-	HttpMethodDelete  = new(httpMethod)
-	HttpMethodOptions = "OPTIONS"
-	textPlain         = new(httpContentType)
-	applicationJson   = new(httpContentType)
+	textPlain       = new(httpContentType)
+	applicationJson = new(httpContentType)
 )
 
 func init() {
-	*HttpMethodGet = "GET"
-	*HttpMethodPost = "POST"
-	*HttpMethodPut = "PUT"
-	*HttpMethodDelete = "DELETE"
 	*textPlain = "text/plain"
 	*applicationJson = "application/json"
 }
