b1424aa1cabf874b3eec87589725fe47504b22cf
vshou <vshowc@outlook.com>
2023-06-28 17:48:52 +0800

    Routing Group Functional Design

--- snow/route.go
+++ snow/route.go
@@ -3,26 +3,31 @@
 package snow
 
 import (
+	"github.com/fine-snow/finesnow/constant"
 	"github.com/fine-snow/finesnow/router"
 	"net/http"
 )
 
 // Get Add a routing method for GET request
 func Get(url string, fun any) {
-	router.AddRoute(url, http.MethodGet, fun)
+	router.AddRoute(constant.NullStr, url, http.MethodGet, fun)
 }
 
 // Post Add a routing method for POST request
 func Post(url string, fun any) {
-	router.AddRoute(url, http.MethodPost, fun)
+	router.AddRoute(constant.NullStr, url, http.MethodPost, fun)
 }
 
 // Put Add a routing method for PUT request
 func Put(url string, fun any) {
-	router.AddRoute(url, http.MethodPut, fun)
+	router.AddRoute(constant.NullStr, url, http.MethodPut, fun)
 }
 
 // Delete Add a routing method for DELETE request
 func Delete(url string, fun any) {
-	router.AddRoute(url, http.MethodDelete, fun)
+	router.AddRoute(constant.NullStr, url, http.MethodDelete, fun)
+}
+
+func Group(url string) router.RouteGroup {
+	return router.NewGroup(url)
 }
