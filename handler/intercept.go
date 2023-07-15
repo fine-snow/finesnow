ecada6774312b32c05926a49ad7117a08097448d
vshou <vshowc@outlook.com>
2023-07-15 10:14:14 +0800

    The interceptor function code is merged into handler.go and add the functionality of the post processor

--- handler/intercept.go
+++
@@ -1,9 +0,0 @@
-// Interceptor
-
-package handler
-
-import "net/http"
-
-// Interceptor Abstract Method
-// Returning true indicates a release request, while false indicates the opposite.
-type Interceptor func(http.ResponseWriter, *http.Request) bool
